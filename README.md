a5cf84315dd99f065cad0467d0fc76d2324dd0e1
Sjoerd Hekking <marcoburgman4@gmail.com>
2021-05-04 23:34:37 +0200

    ULTRA refractor by TRBRY
    MUCH LOVE TRBRY

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SillySpooder
 To generate silly inside joxx 
 quick link: https://sjoerdhekking.github.io/SillySpooder/
+Rewritten by: TRBRY
+Note: Cycy, we lob yu
---
+++ store.js
@@ -0,0 +1,235 @@
+// re-written by TRBRY
+// Var voor strings kinky
+export const QuoteText = [
+    // TODO: fixi fix this editing and proofing
+    `There are many :insertadjective: ways to choose what to read. <br>
+    First, you should ask for recommendations perhaps from :Cycy: and
+    :insertpluralnoun:. <br><br>
+    Whatever you do, don't ask :Gwen: whom only reads :insertadjective2: books
+    with :insertanimal:s on the cover!<br><br>
+    If your friends and family are no help, try checking out the
+    :insertnoun2: Review in <em>The :insertcity: Times</em>.<br><br>
+    If the :insertpluralnoun2: featured there are too :insertadjective3: for your
+    taste, try something a little more low-:insertbodypart:, like :Cycy: would do.
+    You could also choose the :insertadjective4:-fashioned way. Head to
+    your local library or :insertplace: and browse the shelves until something
+    catches your :insertbodypart2:.<br><br>
+    Or, you could save yourself a whole lot of
+    :insertadjective5: trouble and log on :insertwebsite:, the :insertadjective6:
+    new website to :insertverb: for books! With all the time you will save not
+    having to search for :insertpluralnoun3: you can read :insertnumber:
+    more books!`,
+  
+  
+    `After hiding the :insertnoun: in :Cycy:s :insertbodypart:
+    for :insertnumber: years, :Gwen: grew :insertadjective: and tried to
+    sell it to a/an :insertanimal: in :insertcity:, but was caught.`,
+  
+  
+    `:Cycy: was walking in the park one day, humming to themselves.
+    The day was bright, the sun was shining, and the :insertanimal:s were singing.
+    <br><br> Suddenly, a :insertanimal2: ran by.
+    <br><br> Run! Run! the :insertanimal2: cried. :Gwen: is coming!
+    And :Gwen: is really angry about the :insertevent:!`,
+  
+  
+    // TODO: fixi fix this editing and proofing
+    `Dear :Cycy:,<br> :Gwen: is having a :inserttheme: party!
+     It is going to be at the :insertplace: on :insertweekday:.
+     Please make sure to show up, or else you will be required to
+     :insertverb: a/an :insertanimal: with your :insertbodypart:.`,
+  
+  
+    `Twinkle, twinkle, :insertadjective: :Cycy:. <br>
+    How I :insertverb: what you :insertverb2:.<br>
+    Up above the :insertnoun: so :insertadjective2:.<br>
+    Like a :insertnoun2: in the :insertnoun3:.<br>
+    Twinkle, twinkle, :insertadjective: :Cycy:.<br><i>
+    How I :insertverb: what you :insertverb2:.</i><br>
+    Yours :Gwen:!`,
+  
+  
+    `A :insertadjective: fairy princess movie is coming out soon!
+    It will feature Snow-:Cycy: and the :insertnumber: dwarfs.<br><br>
+    Synopsis:<br>
+    Snow-:Cycy: is a princess whose beauty challanges the evil queen. Hence
+    :Cycy: is forced to flee from :insertcity:-castle and hides in the
+    :insertplace:. There, she discovers the dwarfs playing with their
+    :insertpluralnoun:.<br>
+    But the vengeful queens search is not in vain she can casts a
+    :insertadjective2: spell.<br>
+    Catched in the :insertadjective2: spell until the :insertadjective3: :Gwen:
+    comes to rescue her, <strong>will wedding bells ring? come see!</strong>`,
+  
+  
+    `Here are some things to do at recess.
+     <ol>
+     <li> Start a game of touch :insertbodypart:-ball with :Cycy:.</li>
+     <li> Put a :insertnoun: in :Gwen:s lunch bag.</li>
+     <li> Start a :insertvegetable: fight in the school
+      :insertadjective:room against :Cycy:.</li>
+     <li> Choose sides and have a :insertverb:ball
+     tournament versus :Gwen:s team.</li>
+     <li> Demand more :insertpluralnoun: and shorter
+     :insertadjective2: classes with :Gwen:.</li>
+     <li> Choose :Cycy: to be <strong>it</strong>.</li>
+     </ol>`,
+  
+  
+    `Dear :Cycy:, :insertpluralnoun: are :insertcolor:, :insertpluralnoun2: 
+    are blue, you love me and I love :insertBluRhyme:!<br>your :Gwen:.`,
+  
+  
+    `Please excuse :Cycy: who is far too :insertadjective: to attend 
+    :insertnoun: class.<br> Signed by :Gwen:`,
+  
+  
+    `:Cycy: is sick with :insertIllness:. Drink more :insertfluid: 
+    and take :insertsubstance: as needed.<br> - Dr.:Gwen:`,
+  
+  
+    `:Cycy: is authorized to be at the :insertplace: instead of :insertnoun: 
+    class.<br> Signed by :Gwen:`,
+  
+  
+    `:Gwen: and :Cycy: are too :insertadjective: for :insertnoun: class.  
+    Instead they will be attending the :insertevent:.`,
+  ];
+  
+  // :insertadjective: + 2,3,4,5
+  export const adjective = [
+    'caring', 'eager', 'tired', 'friendly', 'glossy', 'helpless', 'ill',
+    'irate', 'lavish', 'quirky', 'rhetorical', 'scandalous', 'sincere',
+    'spotless', 'clever', 'cool',
+  ];
+  
+  // :insertnoun: + 2,3
+  export const noun = [
+    'array', 'badger', 'bracket', 'Harlowe', 'Sugarcube', 'reindoctrination',
+    'Twine', 'interface', 'knitting', 'hoarding', 'mayonnaise', 'octopus', 'pike',
+    'rail', 'scissors', 'cake', 'gaming', 'turkey', 'work',
+  ];
+  
+  // :insertcity:
+  export const city = [
+    'Aberdeen', 'Prune', 'Berlin', 'Burlington', 'Malmö', 'Danbury',
+    'Elk Grove', 'Aarhus', 'GreenBay', 'Glasgow', 'Solingen',
+    'Lakeland', 'Valencia', 'Miami', 'Naples', 'Marseille', 'Orlando',
+    'Pomona', 'Reading', 'Saint Petersburg', 'Milano', 'Springdale',
+    'Miskolc', 'Waco', 'Rotterdam',
+  ];
+  
+  // :insertverb:
+  export const verb = [
+    'accept', 'balance', 'bubble', 'comb', 'damage', 'drown', 'fence', 'grip',
+    'improve', 'label', 'mend', 'owe', 'post', 'reach', 'rob', 'shiver', 'spot',
+    'suppose', 'treat', 'water',
+  ];
+  
+  // :insertanimal: + 2
+  export const animal = [
+    'Aardvark', 'Armadillo', 'Bee', 'Cassowary', 'Chinchilla', 'Crab',
+    'Dogfish', 'Eagle', 'Ferret', 'Gazelle', 'Goose', 'Hare', 'Human',
+    'Jellyfish', 'Lark', 'Louse', 'Meerkat', 'Mule', 'Ostrich', 'Pelican',
+    'Quail', 'Raven', 'Sand Dollar', 'Shrew', 'Squirrel', 'Tarsier', 'Vulture',
+    'Wolverine',
+  ];
+  
+  // :insertpluralnoun: + 2,3
+  export const pluralNoun = [
+    'aardvarks ', 'cars ', 'calculators ', 'coals ', 'diamonds ', 'flutes ',
+    'hats ', 'kayaks ', 'load-screens ', 'otters ', 'undies ', 'irises ',
+    'cuffs ', 'ropes ', 'tortoises ', 'whips ',
+  ];
+  
+  // :insertfluid:
+  export const fluid = [
+    'lood', 'woter', 'IPA', 'orange juice', 'mountain dew', 'coffee',
+    'bubble-tea', 'cola', 'fanta', 'sprite', 'whiskey', 'grass water', 'milk',
+    'hot chocolate', 'blood', 'chai tea - masala spiced',
+  ];
+  
+  // :insertsubstance:
+  export const substance = [
+    'iron', 'sugar', 'soda', 'jello', 'donuts', 'cookies', 'rasgulla', 'salt',
+    'rasin', 'french fries',
+  ];
+  // :insertevent:
+  export const eventthing = [
+    'twine jam', 'skribbl.io tournament', 'dungeon night at Somniums place',
+    'voice channel party', 'sugarcube livestream',
+    'slumber night at :Gwen:s place', 'coding night', 'bulli :Gwen: day',
+    'drawing course with berry', 'game night with redsea', 'bloodsuck day',
+    'nosleep vamp party',
+  ];
+  
+  // :insertweekday:
+  export const weekday = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',
+  ];
+  
+  // :inserttheme:
+  export const theme = [
+    'Harry Potter magic', 'Hollywood Glamour', 'pink flamingo delights',
+    'jungle', 'bright and glowing', 'retro', 'sweet snow cone', 'carnival',
+    'luau time', 'donut', 'sock hop', 'circus galore', 'popsicle',
+    '1001 arabian nights', 'ice cream', 'margarita', 'bring back the eighties',
+    'watermelon', 'taco', 'fortnite', 'coding', 'slumber', 'twinery',
+  ];
+  
+  // what?
+  export const veggie = [
+    'fake fruit', 'apple', 'pear', 'berry', 'banana', 'watermelon', 'tomato',
+    'mushroom', 'orange', 'mango', 'kiwi',
+  ];
+  
+  // :insertbodypart: + 2
+  export const bodyPart = [
+    'eye', 'leg', 'chest', 'finger', 'belly', 'back', 'stomach', 'foot', 'nose',
+    'chin', 'hair', 'neck', 'knee', 'heel', 'cheek', 'tongue', 'lung', 'buttock',
+  ];
+  
+  // :insertplace:
+  export const place = [
+    'Twine Server', 'discord', 'coffee shop', 'submarine', 'Somniums Inn',
+    'Sugarcube Clubhouse', 'The Lost Harlowe', 'Snowman peak', 'Chapbook Lake',
+    'Scary friends home',
+  ];
+  
+  // :insertnumber
+  export const number = [
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+    'ten', 'eleven', 'ninety-six',
+  ];
+  
+  // :insertwebsite:
+  export const website = [
+    'www.twinery.org', 'www.twinelab.net', 'www.discord.com',
+    'wwww.motoslave.com', 'www.github.com',
+  ];
+  
+  // :insertcolor:
+  export const colors = [
+    'white', 'gray', 'black', 'red', 'yellow', 'green', 'purple', 'pink',
+    'orange', 'brown', 'beige',
+  ];
+  
+  // :insetBluRhyme:
+  export const rhymeBlu = [
+    'you', 'Drew', 'brew', 'EU', 'loo', 'roux', 'to', 'who', 'few', 'dew', 'glue',
+    'flu', 'zoo', 'boo', 'goo', 'mew', 'pooh', 'poo', 'gnu', 'anew', 'taboo',
+    'tattoo', 'bamboo', 'canoe', 'undo', 'sinew', 'fondue', 'kazoo', 'revenue',
+    'residue', 'caribou', 'shampoo', 'marabou', 'peekaboo', 'superglue',
+    'barbecue', 'kangaroo', 'cockatoo', 'honeydew',
+  ];
+  // :insertIllness:
+  export const illness = [
+    'Lethargic Osmosis', 'Irascible digestion', 'Intermittent Lachrymation',
+    'Jägoraphobia', 'Entropic Oculitis', 'Hemline Fracture', 'Petulant Agita',
+  ];
+  
+  // twine discord server usual suspects
+  export const suspects = [
+    'Cycy', 'Gwen', 'Redsea', 'Lychgate', 'Jay', 'TRBRY', 'Chapel', 'Greyelf',
+    'TME', 'Somnium', 'Haley Scribe',
+  ];
